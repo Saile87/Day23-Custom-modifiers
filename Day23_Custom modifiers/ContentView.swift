35cd11f315a4a557842b01598978a99107330b2b
Elias Breitenbach <elias.b@me.com>
2023-03-15 21:11:06 +0100

    Pushed project to github

---
+++ Day23_Custom modifiers/ContentView.swift
@@ -0,0 +1,61 @@
+//
+//  ContentView.swift
+//  Day23_Custom modifiers
+//
+//  Created by Elias Breitenbach on 09.03.23.
+//
+
+import SwiftUI
+
+struct Watermark: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomLeading) {
+            content
+            
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(.black)
+        }
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        modifier(Watermark(text: text))
+    }
+}
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        Color.blue
+            .frame(width: 300, height: 200)
+            .watermarked(with: "Hacking with Swift")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day23_Custom modifiers/Day23_Custom_modifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day23_Custom_modifiersApp.swift
+//  Day23_Custom modifiers
+//
+//  Created by Elias Breitenbach on 09.03.23.
+//
+
+import SwiftUI
+
+@main
+struct Day23_Custom_modifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
